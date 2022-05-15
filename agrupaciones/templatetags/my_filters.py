b184b44c51d7a425c603f2f7de0f7d9c43bbf9fb
Jefry <jefrynaicipa@hotmail.com>
2022-05-15 15:30:50 -0500

    FEAT: Se agrega pipe de concurrencia

--- agrupaciones/templatetags/my_filters.py
+++ agrupaciones/templatetags/my_filters.py
@@ -7,3 +7,7 @@ def times(number):
     if number == '':
         number=0
     return range(int(number))
+
+@register.filter(name='currency')
+def currency(number):
+    return "{:,}".format(int(number)).replace(",", ".")
