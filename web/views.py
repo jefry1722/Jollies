f3c1e2e0759af25ee566eb2ad3098b56fcb94e37
Jefry <jefrynaicipa@hotmail.com>
2022-05-15 22:47:03 -0500

    FIX: Se corrige form

--- web/views.py
+++ web/views.py
@@ -3,11 +3,20 @@ from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 
 from agrupaciones.models import Agrupacion
+from agrupaciones.utils import enviarCorreo
 from contrataciones.models import Contratacion
 from usuarios.models import Usuario
 
 
+@require_http_methods(["POST", "GET"])
 def inicio(request):
+    if request.method == "POST":
+        nombre = request.POST.get("name")
+        email = request.POST.get("email")
+        asunto = request.POST.get("subject")
+        mensaje = request.POST.get("message")
+        enviarCorreo("jolliesapp@gmail.com", asunto,
+                     "Usuario: " + nombre + "\nCorreo: " + email + "\nMensaje: " + mensaje)
     agrupaciones = Agrupacion.objects.order_by('id')
     top_agrupaciones = Contratacion.objects.values('agrupacion__nombre').order_by('agrupacion_id').annotate(
         rating=Avg('rating'))[:3]
